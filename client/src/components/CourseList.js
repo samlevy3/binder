7d39f94b4ce82f204548419adf7f6cbf72499b7f
Sam Orth <sam@sorth.com>
2020-08-15 21:44:44 -0700

    Added a header, some stylistic changes for the registration page

--- client/src/components/CourseList.js
+++ client/src/components/CourseList.js
@@ -6,15 +6,18 @@ class CourseList extends React.Component {
     render() {
         if (this.props.courses.length === 0){
             return (
-                <div>
+                <div >
                     No courses added
                 </div>
             )
         } else {
             return (
-                this.props.courses.map((course) => (
-                    <CourseItem key={course} courseName={course} deleteCourse={this.props.deleteCourse}/>
-                ))
+                <div>
+                {
+                    this.props.courses.map((course) => (
+                        <CourseItem key={course} courseName={course} deleteCourse={this.props.deleteCourse}/>
+                ))}
+                </div>
             )
         }
   }
