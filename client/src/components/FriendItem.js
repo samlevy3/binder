518a6a9f1940dc50a4d82a03d3d37672d9262f9e
samuellevy <slevy3@uw.edu>
2020-08-16 10:22:47 -0700

    final push

--- client/src/components/FriendItem.js
+++ client/src/components/FriendItem.js
@@ -4,7 +4,12 @@ class FriendItem extends Component {
     render() {
         return (
             <div>
-                <p style={style}>{this.props.member.name}</p>
+                <p style={style}>
+                    Name: {this.props.member.name}<br></br>
+                    Email: {this.props.member.email}<br></br>
+                    Phone Number: {this.props.member.phone}
+                </p>
+
             </div>
         );
     }
